7f299048aa7417918ecdb364147d36f636314acc
xxqq <xieqi97@163.com>
2022-04-11 22:52:42 +0800

    test: 💍 推送测试

--- src/views/person/index.vue
+++ src/views/person/index.vue
@@ -2,11 +2,11 @@
  * @Description: 
  * @Author: xq
  * @Date: 2022-04-10 22:29:41
- * @LastEditTime: 2022-04-11 22:50:22
+ * @LastEditTime: 2022-04-11 22:52:32
  * @LastEditors: xq
 -->
 <template>
-  <div>个人中心test</div>
+  <div>个人中心test成功</div>
 </template>
 
 <script setup lang="ts"></script>
