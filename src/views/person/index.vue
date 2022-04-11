0fd925e8ebdd8090144e362eab8c098b39f19ad6
xxqq <xieqi97@163.com>
2022-04-11 22:50:37 +0800

    test: 💍 推送测试

--- src/views/person/index.vue
+++ src/views/person/index.vue
@@ -2,11 +2,11 @@
  * @Description: 
  * @Author: xq
  * @Date: 2022-04-10 22:29:41
- * @LastEditTime: 2022-04-10 23:48:37
+ * @LastEditTime: 2022-04-11 22:50:22
  * @LastEditors: xq
 -->
-<template> 
-  <div>个人中心test</div> 
+<template>
+  <div>个人中心test</div>
 </template>
 
 <script setup lang="ts"></script>
