88eff124cce185aaa054665baa6cbc2784b82d35
xxqq <xieqi97@163.com>
2022-04-13 22:53:29 +0800

    test: 💍 test

--- src/views/person/index.vue
+++ src/views/person/index.vue
@@ -2,11 +2,11 @@
  * @Description: 
  * @Author: xq
  * @Date: 2022-04-10 22:29:41
- * @LastEditTime: 2022-04-13 22:18:56
+ * @LastEditTime: 2022-04-13 22:53:21
  * @LastEditors: xq
 -->
 <template>
-  <div>个人中心test成功！</div>
+  <div>个人中心test成功！!</div>
 </template>
 
 <script setup lang="ts"></script>
