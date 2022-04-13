de7f40f007701826579aed028b80a345cee0be51
xxqq <xieqi97@163.com>
2022-04-13 21:56:11 +0800

    feat: 🎸 404界面

--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: xq
  * @Date: 2022-04-10 11:40:22
- * @LastEditTime: 2022-04-10 12:01:49
+ * @LastEditTime: 2022-04-13 21:55:41
  * @LastEditors: xq
 -->
 <template>
@@ -14,7 +14,7 @@
       class="wrapper"
     >
       <template #footer>
-        <n-button>找点乐子吧</n-button>
+        <router-link to="/"><n-button>找点乐子吧</n-button></router-link>
       </template>
     </n-result>
   </div>
