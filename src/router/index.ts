befc0bfd02b63cfcce597f43fca98875a5a515b0
xxqq <xieqi97@163.com>
2022-04-13 21:20:04 +0800

    feat: 🎸 增加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: xq
  * @Date: 2022-04-06 21:49:10
- * @LastEditTime: 2022-04-12 22:04:18
+ * @LastEditTime: 2022-04-13 21:19:53
  * @LastEditors: xq
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
@@ -20,5 +20,13 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    next('/dashboard')
+  } else {
+    next()
+  }
+  // if(to)
+})
 export default router
