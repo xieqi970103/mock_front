603e663c8c2af4d5b91fac90392b63fbbe53b498
xxqq <xieqi97@163.com>
2022-05-03 11:59:40 +0800

    test: 💍 test

--- src/views/ruleManage/index.vue
+++ src/views/ruleManage/index.vue
@@ -2,7 +2,7 @@
  * @Description: 规则管理
  * @Author: xq
  * @Date: 2022-04-10 20:41:02
- * @LastEditTime: 2022-05-03 11:33:16
+ * @LastEditTime: 2022-05-03 11:59:34
  * @LastEditors: xq
 -->
 <template>
@@ -75,7 +75,7 @@ const columns = reactive(
 
 /* 获取数据 */
 const data: Song[] = [
-  { no: 3, title: 'Wonderwall——test', length: '4:18' },
+  { no: 3, title: 'Wonderwall_test', length: '4:18' },
   { no: 4, title: "Don't Look Back in Anger", length: '4:48' },
   { no: 12, title: 'Champagne Supernova', length: '7:27' },
 ]
